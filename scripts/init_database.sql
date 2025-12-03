eb039f9202c20df22dd88c7a36193f63339bddb1
Om Pandurang Gonjari <omgonjari2004@gmail.com>
2025-12-03 11:57:17 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,13 @@
+-- Create Database 'DataWarehouse'
 
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
